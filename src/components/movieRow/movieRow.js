a1889e060bf77af4c8f2c47d109ecb6fd3de57f2
Trisha Dring <trisha.dring@gmail.com>
2021-07-20 11:28:20 -0400

    movie titles fixed to bottom

--- src/components/movieRow/movieRow.js
+++ src/components/movieRow/movieRow.js
@@ -1,13 +1,14 @@
 import styles from "./movieRow.css";
 
 const MovieRow = ({film}) => {
-   const posterUrl = 'https://image.tmdb.org/t/p/w780/' + film.poster_path;
-
+   const posterUrl = 'https://image.tmdb.org/t/p/w780/' + film.poster_path; 
   return(
-  <div >
-    <img src={posterUrl} alt='img' className='movieImg'/>
-    <p className="Title">{film.title}</p>
-    <p className="score">{film.vote_average}</p>
+  <div class="contain" >
+    <img className='movieImg' src={posterUrl} alt='img' />
+    <div className="movieTitles">
+      <p className="Title">{film.title}</p>
+      {/* <p className="score">{film.vote_average}</p> */}
+    </div>
   </div>
   )
 }
